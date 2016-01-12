f8aefc9cfbf917a908945bf760aa66197f61be97
sushant-maragaje <sushant.maragaje4@gmail.com>
2016-01-12 11:49:08 +0530

    12-01-2015

--- MSPowerWebApp/Models/ImageUploadViewModel.cs
+++ MSPowerWebApp/Models/ImageUploadViewModel.cs
@@ -3,16 +3,23 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using System.Web.Mvc;
 
 namespace MSPowerWebApp.Models
 {
     public class ImageUploadViewModel
     {
+
+        //Dictionary<string, string> dictionary = new Dictionary<string, string>();
+
+        public List<string> Images = new List<string>();
+
         public ImageUploadViewModel()
         {
             Friendly_Message = new List<FriendlyMessageInfo>();
         }
 
         public List<FriendlyMessageInfo> Friendly_Message { get; set; }
+
     }
 }
